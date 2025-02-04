193426da7f74ae3a9f3dc1ab594056a17bae17d2
jungminji <wjdalswl0215@gmail.com>
2025-02-04 21:00:54 +0900

    refac : MyProfile 컴포넌트 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-import Image from "next/image";
-import profileImage from "../../public/minji.jpeg";
+import MyProfile from "./components/MyProfile";
 export default function Home() {
   return (
-    <main>
-      <section className="flex ms:flex-col bg-green-200 justify-center gap-5 p-5">
-        <Image src={profileImage} alt="프로필 사진" width={200} height={200} priority className="rounded-full" />
-        <div className="flex flex-col justify-center">
-          <p>안녕하세요.</p>
-          <p>개발 공부 내용을 기록합니다. ✏️</p>
-          <p className="text-gray-300 text-sm cursor-pointer mt-3">🌼 저의 사이트에 놀러오세요 🌼</p>
-        </div>
-      </section>
+    <main className="bg-yellow-200">
+      <MyProfile />
     </main>
   );
 }
---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PostDetailPage() {
+  return <div>디테일 페이지</div>;
+}
