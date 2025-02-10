bee255a7f810ac712a9becbd4f63194d7f20b7f0
jungminji <wjdalswl0215@gmail.com>
2025-02-10 13:28:03 +0900

    update : 게시글 목록에서 최신글 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Hero from "./components/Hero";
 import Posts from "./components/post/Posts";
-import RecentPost from "./components/post/RecentPost";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <RecentPost />
       <Posts />
     </>
   );
