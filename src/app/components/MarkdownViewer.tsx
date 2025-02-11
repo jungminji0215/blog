110be17a47169e4e0b63a856bfd0b99bb4f19131
jungminji <wjdalswl0215@gmail.com>
2025-02-12 00:53:00 +0900

    style : 상세 페이지 스타일

--- src/app/components/MarkdownViewer.tsx
+++ src/app/components/MarkdownViewer.tsx
@@ -3,7 +3,7 @@ import remarkGfm from "remark-gfm";
 
 export default function MarkdownViewer({ content }: { content: string }) {
   return (
-    <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown className="prose max-w-none" remarkPlugins={[remarkGfm]}>
       {content}
     </ReactMarkdown>
   );
--- src/app/posts/[category]/[slug]/page.tsx
+++ src/app/posts/[category]/[slug]/page.tsx
@@ -12,9 +12,14 @@ export default async function PostDetailPage({ params: { category, slug } }: Pro
   const post = await postApi.getPost(category, slug);
 
   return (
-    <article className="bg-gray-200 max-w-screen-xl m-auto p-5">
-      <h1>{post.title}</h1>
-      <MarkdownViewer content={post.content} />
+    <article className="overflow-hidden max-w-screen-md m-auto">
+      <section className="flex flex-col p-5">
+        <h1 className="font-bold text-4xl border-b border-yellow-500 py-3">{post.title}</h1>
+        <p className="text-gray-500 p-2 text-end">{post.date}</p>
+        <div className="py-5 px-2 mb-5">
+          <MarkdownViewer content={post.content} />
+        </div>
+      </section>
     </article>
   );
 }
