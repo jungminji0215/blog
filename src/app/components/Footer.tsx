81b3a9d80b7c0a31df2dec8ebb17c967e0c3f2f6
jungminji <wjdalswl0215@gmail.com>
2025-02-11 13:50:48 +0900

    style : footer padding 조절

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="p-5 text-center">
+    <footer className="p-10 text-center bg-gray-100">
       <p className="text-sm text-gray-500">Copyright 2025.Jung Minji.All rights rederved.</p>
     </footer>
   );
