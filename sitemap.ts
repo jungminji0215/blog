956aff35a45e5d08a4453803876c34c36bd7a028
jungminji <wjdalswl0215@gmail.com>
2025-03-16 02:44:34 +0900

    add : sitemap.xml

---
+++ sitemap.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from "next";
+import { postApi } from "@/service/posts";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await postApi.getAllPosts();
+
+  return posts.map((post) => ({
+    url: `https://www.jungminji.com/posts/${post.category}/${post.slug}`,
+    lastModified: post.date,
+    changeFrequency: "weekly",
+    priority: 1,
+  }));
+}
