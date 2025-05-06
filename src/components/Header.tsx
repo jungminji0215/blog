c69a9902da346b841f055789f15d32b58e3bc0bd
jungminji <wjdalswl0215@gmail.com>
2025-05-06 23:04:20 +0900

    [refac] header 리팩토링 - 접근성 향상

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,17 @@
-import Link from "next/link";
-import React from "react";
-import { ROUTES } from "../constants/routes";
+import Link from 'next/link';
+import React from 'react';
+import { ROUTES } from '@/constants/routes';
 
 export default function Header() {
   return (
-    <header>
-      <nav className="max-w-screen-xl m-auto p-4 flex justify-between border-b border-gray-200">
+    <header className="border-b border-gray-200">
+      <nav aria-label="주요 탐색" className="m-auto flex max-w-6xl justify-between p-4">
         <div>
-          <Link href={ROUTES.HOME}>
-            <h1 className="text-3xl font-title">{"Minji's Devlog"}</h1>
-          </Link>
+          <h1 className="font-title text-3xl">
+            <Link href={ROUTES.HOME}>Minji’s Devlog</Link>
+          </h1>
         </div>
-        <div className="flex gap-3 items-center font-bold text-2xl">
+        <div className="flex items-center gap-3 text-2xl font-bold">
           {/* <MdOutlineDarkMode /> */}
           {/* <Link href={ROUTES.HOME}>Home</Link> */}
         </div>
