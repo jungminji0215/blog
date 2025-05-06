1931b633c6ff06425349ad43150f0757039531a3
jungminji <wjdalswl0215@gmail.com>
2025-05-06 23:04:52 +0900

    [refac] metadata 수정 - base url 명시

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,31 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import type { Metadata } from 'next';
+import './globals.css';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import React, { ReactNode } from 'react';
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://www.jungminji.com'),
   title: "Minji's Devlog",
-  description: "웹 개발자 정민지의 기술 블로그",
+  description: '정민지의 기술 블로그',
   verification: {
-    google: "8NjREOg8xfvS2aWwB51syCKjsf5dfjg9DljIFEiqJT8",
+    google: '8NjREOg8xfvS2aWwB51syCKjsf5dfjg9DljIFEiqJT8',
   },
   icons: {
-    icon: "/favicon.ico",
+    icon: '/favicon.ico',
   },
   openGraph: {
-    type: "website",
+    type: 'website',
     title: "Minji's Devlog",
-    description: "웹 개발자 정민지의 기술 블로그",
-    url: "https://www.jungminji.com/",
+    description: '정민지의 기술 블로그',
+    url: 'https://www.jungminji.com',
     siteName: "Minji's Devlog",
     images: [
       {
-        url: "/jungminji.png",
+        url: '/hero.png',
         width: 1200,
         height: 630,
-        alt: "jungminji image",
+        alt: '정민지 이미지',
       },
     ],
   },
@@ -32,13 +34,13 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="ko">
       <body className="flex flex-col font-content">
         <Header />
-        <main className="grow mb-20">{children}</main>
+        <main className="mb-20 grow">{children}</main>
         <Footer />
       </body>
     </html>
